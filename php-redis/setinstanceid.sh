d6a03ae0682a750052cb0c16d28373f8aaf1f1f5
zengelan <andreas.zengel@gmail.com>
2020-01-07 23:50:50 +0100

    update instanceid

--- php-redis/setinstanceid.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# Get the instance ID from Amazon AWS endpoint and save in env variable
-INSTANCEID=$(curl --silent http://169.254.169.254/latest/meta-data/instance-id)
-export INSTANCEID
-
-exit 0
